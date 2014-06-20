66a13fbc560b7d20129b3d2622df8f1047096dbe
Esa Varemo <esa@kuivanto.fi>
2014-06-20 21:48:54 +0300

    Added basic MSP comms (poll gps)

--- mspcomms.cpp
+++ mspcomms.cpp
@@ -1,10 +1,87 @@
 #include <QList>
 #include <QStringList>
+#include <QByteArray>
+#include <QDebug>
+
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
+#include <QTimer>
 
 #include "mspcomms.h"
 
-MspComms::MspComms()
+
+MspComms::MspComms(QString portname, QObject *parent) : QObject(parent)
 {
+    port = new QSerialPort(portname, this);
+    port->open(QIODevice::ReadWrite);
+}
+
+void MspComms::startQueryGPS(unsigned int interval_msec) {
+    timer = new QTimer(this);
+    timer->setInterval(interval_msec);
+    timer->setSingleShot(false);
+
+    connect(timer, SIGNAL(timeout()), this, SLOT(sendGPSQuery()));
+    connect(port, SIGNAL(readyRead()), this, SLOT(dataReceived()));
+
+    timer->start();
+}
+
+void MspComms::sendMSPCommand(char command, QByteArray payload = NULL) {
+    QByteArray data = "";
+    char checksum = 0;
+
+    data.append("$M<"); // Header
+
+    if(payload != NULL) {
+        data.append(payload.length());     // Data length
+        checksum ^= payload.length();
+    } else {
+        data.append((char)0);                    // Data length
+    }
+
+    data.append(command);   // Command
+
+    checksum ^= command;
+
+    if(payload != NULL) {
+        for(int i = 0; i < payload.length(); i++) {
+            data.append(payload.at(i));
+            checksum ^= payload.at(i);
+        }
+    }
+
+    data.append(checksum);
+
+    qDebug() << "Sending: ";
+    for(int i = 0; i < data.length(); i++) {
+        if((int)data.at(i) > 31)
+            qDebug() << (uint)data.at(i) << " (" << (char)data.at(i) << ")";
+        else
+            qDebug() << (uint)data.at(i);
+    }
+
+    int written = port->write(data);
+    if(written != data.length()) {
+        qWarning() << "Error writing data";
+    }
+}
+
+void MspComms::sendGPSQuery() {
+    sendMSPCommand(106);
+
+}
+
+void MspComms::dataReceived() {
+    qDebug() << "Received data:";
+    QByteArray data = port->readAll();
+    for(int i = 0; i < data.length(); i++) {
+        if((int)data.at(i) > 31)
+            qDebug() << (uint)data.at(i) << " (" << (char)data.at(i) << ")";
+        else
+            qDebug() << (uint)data.at(i);
+    }
 }
 
 QStringList MspComms::getPortNames() {
--- mspcomms.h
+++ mspcomms.h
@@ -1,16 +1,37 @@
-#include <QtSerialPort>
+#include <QObject>
+
 #include <QString>
 #include <QStringList>
+#include <QTimer>
+
+#include <QSerialPort>
+
 
 #ifndef MSPCOMMS_H
 #define MSPCOMMS_H
 
-class MspComms
+class MspComms : public QObject
 {
+    Q_OBJECT
+
 public:
-    MspComms();
+    explicit MspComms(QString portname, QObject *parent = 0);
+
+    void startQueryGPS(unsigned int interval_msec);
+
+    void sendMSPCommand(char command, QByteArray data);
 
     static QStringList getPortNames();
+private:
+    QSerialPort *port;
+    QTimer *timer;
+
+signals:
+    void newGPSData();
+
+private slots:
+    void sendGPSQuery();
+    void dataReceived();
 };
 
 #endif // MSPCOMMS_H
